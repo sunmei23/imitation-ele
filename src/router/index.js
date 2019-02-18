010290cc969622e13ed1b8c99e79dcb8b83ada37
sunmei23 <sunmei1201>
2019-02-18 17:05:05 +0800

    first version

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import goods from '@/components/goods/goods.vue';
+import comments from '@/components/comments/comments.vue';
+import shops from '@/components/shops/shops.vue';
+Vue.use(Router);
+
+export default new Router({
+  linkActiveClass: 'tab-active',
+  mode: 'history',
+  routes: [
+    // {
+    //   path: '/',
+    //   components: {
+    //     default: goods,
+    //     comments: comments,
+    //     shops: shops
+    //   }
+    // }
+    {
+      path: '/',
+      redirect: { name: 'goods' }
+    },
+    {
+      path: '/goods',
+      name: 'goods',
+      component: goods
+    },
+    {
+      path: '/comments',
+      name: 'comments',
+      component: comments
+    },
+    {
+      path: '/shops',
+      name: 'shops',
+      component: shops
+    }
+  ]
+});
