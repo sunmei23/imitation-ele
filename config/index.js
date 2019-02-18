4c692d6b52cfb6e876b06942f2a0d6f6ad1015f6
sunmei23 <sunmei1201>
2019-02-18 17:40:02 +0800

    在git push 上传 github上后，dist文件没有上传上去，被 .gitignore 忽略了

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: './static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/imitation-ele/dist',
     proxyTable: {},
 
     // Various Dev Server settings
